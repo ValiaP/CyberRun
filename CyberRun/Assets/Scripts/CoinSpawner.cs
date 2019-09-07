5609f0a24a0208f0630bc439f354aa54ed2a7c11
Artur Kozdęba <amazur20@yahoo.com>
2019-09-07 23:51:30 +0200

    Coin Spawner

--- CyberRun/Assets/Scripts/CoinSpawner.cs
+++ CyberRun/Assets/Scripts/CoinSpawner.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour
 {
+    //@Amazur / Mee6
+    //To do:
+    //Generate X coins in a row where X random
+    //After X coins generate obstacle prefab
+    //Switch PosXOffset
+
+
     public GameObject prefab; //coin prefab
     public Vector3 spawnPos; //spawning position of the coin
     public float spawnPosXoffset = 0.7f; //offset of the X of the coin - "which lane it spawns on"
     public float timeOffset = 0.5f; //during what time it spawns
+    public float coinSpeed = 1f; //speed of the coin
+    Rigidbody rb;
     void Start()
     {
         InvokeRepeating("Spawn", 0,timeOffset);
@@ -16,5 +25,7 @@ public class CoinSpawner : MonoBehaviour
     void Spawn()
     {
         GameObject coin = Instantiate(prefab, spawnPos, Quaternion.identity);
+        rb = coin.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, 0, coinSpeed);
     }
 }
