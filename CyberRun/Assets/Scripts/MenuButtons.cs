496996bac68536545c87fb5d853c9cd74461245d
lolgians05 <45824238+lolgians05@users.noreply.github.com>
2019-09-08 13:44:02 +0200

    main menu (all placeholder and stolen background)

---
+++ CyberRun/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void MoveToNextScene()
+    {
+        var currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
